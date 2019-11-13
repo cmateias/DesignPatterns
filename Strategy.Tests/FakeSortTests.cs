b980cfbc3c848e0dc74fe896db5b4f6bea609275
Cris Mat <cristinel.mateias@deptagency.com>
2019-11-13 14:09:19 +0000

    composite

---
+++ Strategy.Tests/FakeSortTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+
+namespace Strategy.Tests
+{
+    public class FakeSortTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+
+        }
+
+        [Test]
+        public void When_Sort_IsRequested_Then_ConsoleWritesOut()
+        {
+            //setup
+            List<string> fakeList = new List<string>();
+
+            var sut = new FakeSort();
+            string expectedOutput = $"FakeSort called{Environment.NewLine}";
+
+            //action
+            using (var consoleOutput = new ConsoleOutput())
+            {
+                sut.Sort(fakeList);
+
+                //assert
+                Assert.AreEqual(expectedOutput, consoleOutput.GetOuput());
+            }
+        }
+    }
+}
