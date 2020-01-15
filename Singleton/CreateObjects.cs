bc7ab257ba4a067bb3ccc774d26b0325e08e1519
Cris Mat <cristinel.mateias@deptagency.com>
2020-01-15 17:12:07 +0000

    singleton

---
+++ Singleton/CreateObjects.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    public class CreateObjects
+    {
+        public static void Run()
+        {
+            Parallel.Invoke(
+                () => PrintTeacherDetails(),
+                () => PrintStudentdetails()
+            );
+            Console.ReadLine();
+        }
+
+        private static void PrintTeacherDetails()
+        {
+            Singleton fromTeacher = Singleton.GetInstance;
+            fromTeacher.PrintDetails("From Teacher");
+        }
+        private static void PrintStudentdetails()
+        {
+            Singleton fromStudent = Singleton.GetInstance;
+            fromStudent.PrintDetails("From Student");
+        }
+    }
+}
---
+++ Singleton/Singleton.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Singleton
+{
+    public sealed class Singleton
+    {
+        private static int counter = 0;
+        private static Singleton instance = null;
+        private static readonly object Instancelock = new object();
+
+        public static Singleton GetInstance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    lock (Instancelock)
+                    {
+                        if (instance == null)
+                            instance = new Singleton();
+                    }
+                }
+                return instance;
+            }
+        }
+
+        private Singleton()
+        {
+            counter++;
+            Console.WriteLine("Counter Value " + counter.ToString());
+        }
+        public void PrintDetails(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+    }
+}
