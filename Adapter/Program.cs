19add8ad60902025bacd33e7dc4b0e560feb92e9
Cris Mat <cristinel.mateias@deptagency.com>
2020-01-16 08:10:30 +0000

    Adapter

---
+++ Adapter/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Adapter
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            CreateObjects.Run();
+        }
+    }
+}
---
+++ Adapter/ThirdPartyBillingSystem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Adapter
+{
+    public class ThirdPartyBillingSystem
+    {
+        public void ProcessSalary(List<Employee> listEmployee)
+        {
+            foreach (Employee employee in listEmployee)
+            {
+                Console.WriteLine("Rs." + employee.Salary + " Salary Credited to " + employee.Name + " Account");
+            }
+        }
+    }
+}
