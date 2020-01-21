a750d811602a5bb55a99e91208e5a5598d3054b8
Cris Mat <cristinel.mateias@deptagency.com>
2020-01-21 13:51:04 +0000

    facade

---
+++ Facade/CreateObject.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Facade
+{
+    public class CreateObject
+    {
+        public static void Run()
+        {
+            Order order = new Order();
+            order.PlaceOrder();
+            Console.Read();
+        }
+    }
+}
